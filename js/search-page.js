62c49cb16158595cc18bd6800626471024cef012
ggong00 <ggo0415@naver.com>
2022-08-07 19:53:47 +0900

    검색페이지 버그 수정

--- js/search-page.js
+++ js/search-page.js
@@ -1,44 +1,81 @@
 // dom접근
-const $selectedCg = document.querySelector('.search-wrap__cg-wrap');
+const $selectedCg = document.querySelector('.selected-wrap__cg-wrap');
 const $cgLists = document.querySelector('.search__select-input');
 const $btnSearch = document.querySelector('.btn-search');
 const $btnReset = document.querySelector('.btn-reset');
-const searchedText = document.querySelector('.selected-wrap .searched-text');
-
+let $btnAddContentsByFa = document.querySelector('.searched-text .btn-search-upda');
+const $selectedWrap = document.querySelector('.selected-wrap');
+const $searchedTextWrap = document.querySelector('.selected-wrap .searched-text');
+const searchedText = document.querySelector('.selected-wrap .searched-text p');
+const $inputByText = document.getElementById('textSearchInput')  
 //선택된 카테고리 배열
-const selectedCgArr = [];
+const selectedCgArrSave = [];
+let searchedTextSave = '';
 
+// 시설명 검색 클릭 이벤트
+$inputByText.addEventListener('click', () => {
+  $searchedTextWrap.classList.remove('visibility_visible');
+  $btnAddContentsByFa.classList.add('visibility_visible');
+})
+
+// 시설명 추가버튼 클릭 이벤트
+$btnAddContentsByFa.addEventListener('click', () => {
+  let value = $inputByText.value.trim();
+  if (value != '') {
+    searchedTextSave = value;
+    $searchedTextWrap.classList.add('visibility_visible');
+    if(value.length >= 11) {
+      searchedText.textContent = value.substr(0,10) + '...';
+    } else {
+      searchedText.textContent = value;
+    }
+  } else {
+    searchedTextSave = '';
+  }
+  $inputByText.value = '';
+  $btnAddContentsByFa.classList.remove('visibility_visible');
+})
+
+$selectedWrap.addEventListener('mouseleave', () => {
+  $btnAddContentsByFa.classList.remove('visibility_visible');
+}) 
+
+
+// 카테고리 클릭 이벤트
 $cgLists.addEventListener('click', ({target}) => {
-  // p태그 클릭시 이벤트 진행
+  // p태그 판별
   if(!(target.tagName == 'P')) return;
 
   // 최대 갯수 제한
-  if(selectedCgArr.length >= 10) {
+  if(selectedCgArrSave.length >= 10) {
     alert('최대 10개 선택가능합니다.');
     return;
   }
 
   // 중복 카테고리 제한
-  if(selectedCgArr.indexOf(target.textContent) != -1) {
+  if(selectedCgArrSave.indexOf(target.textContent) != -1) {
     alert('이미 선택된 카테고리입니다');
     return;
   }
 
   target.classList.add('cg-active');
 
+
   // 태그 생성
   const selectedCg_div = document.createElement('div');
+  console.log(selectedCg_div);
+
   const selectedCg_label = document.createElement('label');
   const selectedCg_btn = document.createElement('button');
 
   selectedCg_div.setAttribute('class','selected-cg');
   selectedCg_btn.setAttribute('type','button');
   selectedCg_btn.setAttribute('class','btn-close shadow-none');
-  selectedCg_btn.setAttribute('id','btnClose');
+  selectedCg_btn.setAttribute('id',`btnClose_${target.textContent}`);
   selectedCg_btn.setAttribute('aria-label','Close');
-  selectedCg_label.setAttribute('for','btnClose')
+  selectedCg_label.setAttribute('for',`btnClose_${target.textContent}`)
   selectedCg_label.textContent = target.textContent;  
-  
+
   // 버튼 클릭 이벤트
   selectedCg_btn.addEventListener('click', () => {
     selectedCg_div.style.transition = 'all 0.4s';
@@ -47,64 +84,53 @@ $cgLists.addEventListener('click', ({target}) => {
     setTimeout(() => {
       selectedCg_div.remove();
       target.classList.remove('cg-active');
-      selectedCgArr.forEach((ele,idx) => {
+      selectedCgArrSave.forEach((ele,idx) => {
         if(ele == target.textContent ) {
-          selectedCgArr.splice(idx,1);
+          selectedCgArrSave.splice(idx,1);
         }
       })
     }, 300);
   })
-
+  
   // 태그 삽입
   selectedCg_div.appendChild(selectedCg_label);
   selectedCg_div.appendChild(selectedCg_btn);
   $selectedCg.appendChild(selectedCg_div);
-  selectedCgArr.push(selectedCg_label.textContent);
+  selectedCgArrSave.push(selectedCg_label.textContent);
+
 })
 
-// 검색버튼 클릭
+// 검색버튼 클릭 이벤트
 $btnSearch.addEventListener('click',e => {
-  const inputBytext = document.querySelector('.search__text-input input[type=text]');
-
-  const value = inputBytext.value.trim();
+  const value = searchedTextSave.trim();
 
   if(value == '') {
     // 카테고리 검색
-
-
-
-    searchedText.classList.remove('opacity_on');
-    console.log(selectedCgArr);  
+    console.log(selectedCgArrSave);  
 
   } else {
-    // 시설명 + 카테고리 검색
-    if(value.length >= 10) {
-      searchedText.textContent = inputBytext.value.substr(0,9) + '...';
-    } else {
-      searchedText.textContent = inputBytext.value;
-    }
+    // 카테고리 + 시설명 검색
 
-    searchedText.classList.add('opacity_on');
-  
-    console.log(inputBytext.value);
-    console.log(selectedCgArr);
+    console.log(searchedTextSave)
+    console.log(selectedCgArrSave);
     
   }
-
-  inputBytext.value = '';
 })
 
-// 리셋버튼 클릭
+// 리셋버튼 클릭 이벤트
 $btnReset.addEventListener('click', e => {
   [...$selectedCg.querySelectorAll('.selected-cg')]
               .forEach(ele => ele.remove());
-  selectedCgArr.splice(0,selectedCgArr.length);            
+
+  selectedCgArrSave.splice(0,selectedCgArrSave.length);            
 
   document.querySelectorAll('.category .cg-active')
           .forEach(ele => {
             ele.classList.remove('cg-active');
-          })
+          }) 
 
-  searchedText.classList.remove('opacity_on');
+  searchedTextSave = '';        
+
+  $searchedTextWrap.classList.remove('visibility_visible');
 
 });
