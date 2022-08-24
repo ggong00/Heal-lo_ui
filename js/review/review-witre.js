45c48b27b16b6fa9a301adc00825e3bcfff32ad1
ggong00 <ggo0415@naver.com>
2022-08-24 18:10:25 +0900

    리뷰페이지 사진업로드 추가

--- js/review/review-witre.js
+++ js/review/review-witre.js
@@ -1,13 +1,46 @@
 //dom접근
-const changeScore = document.querySelector('.change-score');
+const $changeScore = document.querySelector('.change-score');
+const $inputImg = document.querySelector('.input-img');
+const $previewImg = document.querySelector('.preview-img');
 
 let ratingScore = 0;
+let uploadImgs = [];
 
 //별점 변경 이벤트
-changeScore.addEventListener('input',({target}) => {
+$changeScore.addEventListener('input',({target}) => {
     const value = target.value
     document.querySelector('.inner-star').style.width = `${target.value * 10}%`;
     ratingScore = parseInt(value)/2;
 
     document.querySelector('.text-score').textContent = ratingScore;
 })
+
+//이미지 업로드 이벤트
+$inputImg.addEventListener('change',({target}) => {
+    const files = target.files;
+    if(!target.files && !target.files[0]) return;
+    const notSupportFile = [...files].find(ele => ele.type != 'image/png' && ele.type != 'image/jpeg');
+    if(notSupportFile) {
+        alert('지원하지 않는 파일')
+        return;
+    }
+    if(uploadImgs.length+target.files.length > 5) {
+        alert('파일 초과');
+        return;
+    }
+
+    [...files].forEach(ele => {
+        uploadImgs.push(ele);
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            console.log(ele.type)
+            const img = document.createElement('img');
+            img.setAttribute('src',e.target.result);
+            $previewImg.appendChild(img);
+        }
+        reader.readAsDataURL(ele);
+    });
+})
+
+
